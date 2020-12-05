87354471b0067536f4dbbfe34fe02e51d9b33c31
Santiago Andrés Ardiles Páez <santiagoandres@outlook.de>
2020-12-05 23:11:49 +0100

    Python string methods.

--- computer-science/strings/strings-methods.py
+++ computer-science/strings/strings-methods.py
@@ -1,5 +1,6 @@
 """
-# Formatting methods.
+# Upper, lower, and title.
+
     poem_title = "spring storm"
     poem_author = "William Carlos Williams"
 
@@ -13,13 +14,15 @@
 
 """
 # Splitting strings.
-    line_one = "The sky has given over"
-    line_one_words = line_one.split()
+
+    i_one = "The sky has given over"
+    i_one_words = i_one.split()
 """
 
 
 """
 # Splitting strings 2.
+
     authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
     author_names = authors.split(",")
 
@@ -38,14 +41,16 @@
 
 """
 # Joining strings.
-    reapers_line_one_words = ["Black", "reapers", "with", "the", "sound", "of", "steel", "on", "stones"]
-    reapers_line_one = " ".join(reapers_line_one_words)
+
+    reapers_i_one_words = ["Black", "reapers", "with", "the", "sound", "of", "steel", "on", "stones"]
+    reapers_i_one = " ".join(reapers_i_one_words)
 """
 
 
 """
 # Joining strings 2.
-    winter_trees_lines = [
+
+    winter_trees_is = [
         "All the complicated details",
         "of the attiring and",
         "the disattiring are completed!",
@@ -58,13 +63,14 @@
         "stand sleeping in the cold.",
     ]
 
-    winter_trees_full = "\n".join(winter_trees_lines)
+    winter_trees_full = "\n".join(winter_trees_is)
 """
 
 
 """
 # Stripping.
-    love_maybe_lines = [
+
+    love_maybe_is = [
         "Always    ",
         "     in the middle of our bloodiest battles  ",
         "you lay down your arms",
@@ -73,12 +79,12 @@
         "   to conquer me home.    ",
     ]
 
-    love_maybe_lines_stripped = []
+    love_maybe_is_stripped = []
 
-    for line in love_maybe_lines:
-        love_maybe_lines_stripped.append(line.strip())
+    for i in love_maybe_is:
+        love_maybe_is_stripped.append(i.strip())
 
-    love_maybe_full = "\n".join(love_maybe_lines_stripped)
+    love_maybe_full = "\n".join(love_maybe_is_stripped)
 
     print(love_maybe_full)
 """
@@ -86,6 +92,7 @@
 
 """
 # Replacing.
+
     toomer_bio = "Nathan Pinchback Tomer, who adopted the name Jean Tomer early in his literary career, was born in Washington, D.C. in 1894. Jean is the son of Nathan Tomer was a mixed-race freedman, born into slavery in 1839 in Chatham County, North Carolina. Jean Tomer is most well known for his first book Cane, which vividly portrays the life of African-Americans in southern farmlands."
 
     toomer_bio_fixed = toomer_bio.replace("Tomer", "Toomer")
@@ -94,21 +101,26 @@
 
 """
 # Finding.
-    god_wills_it_line_one = "The very earth will disown you"
 
-    disown_placement = god_wills_it_line_one.find("disown")
+    god_wills_it_i_one = "The very earth will disown you"
+
+    disown_placement = god_wills_it_i_one.find("disown")
 """
+
 
 """
 # Formatting.
+
     def poem_title_card(title, poet):
         return 'The poem "{}" is written by {}.'.format(title, poet)
 
     poem_title_card("I Hear America Singing", "Walt Whitman")
 """
 
+
 """
 # Formatting 2.
+
     def poem_description(publishing_date, author, title, original_work):
         poem_desc = "The poem {title} by {author} was originally published in {original_work} in {publishing_date}.".format(
             publishing_date=publishing_date,
@@ -122,4 +134,49 @@
     my_beard_description = poem_description(
         "1974", "Shel Silverstein", "My Beard", "Where the Sidewalk Ends"
     )
+"""
+
+
+"""
+# Review
+
+    highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
+
+    # First step.
+        highlighted_poems_list = highlighted_poems.split(",")
+
+
+    # Second step.
+        highlighted_poems_stripped = []
+
+        for i in highlighted_poems_list:
+            highlighted_poems_stripped.append(i.strip())
+
+
+    # Third step.
+        highlighted_poems_details = []
+
+        for i in highlighted_poems_stripped:
+            highlighted_poems_details.append(i.split(":"))
+
+    # Fourth step.
+        titles = []
+        poets = []
+        dates = []
+
+        for i in highlighted_poems_details:
+            for k in range(len(i)):
+                if k == 0:
+                    titles.append(i[k])
+                elif k == 1:
+                    poets.append(i[k])
+                else:
+                    dates.append(i[k])
+
+
+    # Fifth step.
+        for i in range(len(titles)):
+            print(
+                "The poem '{}' was published by {} in {}.".format(titles[i], poets[i], dates[i])
+            )
 """
