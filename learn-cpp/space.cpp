9f4b1006c13c5dd956ea6cc5a9535e126858d2b9
Santiago Andrés Ardiles Páez <santiagoandres@outlook.de>
2020-12-06 00:19:02 +0100

    Improving yet again space.pp

--- learn-cpp/space.cpp
+++ learn-cpp/space.cpp
@@ -1,59 +1,68 @@
 #include <iostream>
+#include <stdlib.h>
+#include <string>
 
 int main()
 {
-    double weight;
     int planet;
+    double weight;
+    std::string planet_name;
+
+    // Change to "cls" for Windows.
+    system("clear");
 
     std::cout << "Enter you weight on Earth: ";
     std::cin >> weight;
 
     std::cout << "\nI have information for the following planets:\n\n";
-    std::cout << "   1. Venus   2. Mars    3. Jupiter\n";
-    std::cout << "   4. Saturn  5. Uranus  6. Neptune\n\n";
+    std::cout << "   1. Mercury   2. Venus    3. Mars\n";
+    std::cout << "   4. Jupiter   5. Saturn   6. Uranus\n";
+    std::cout << "   7. Neptune\n\n";
 
-    std::cout << "Enter the number of the planet: ";
+    std::cout << "Which planet are you visiting? ";
     std::cin >> planet;
 
     switch (planet)
     {
     case 1:
         weight = weight * 0.38;
-        std::cout << weight << "\n";
+        planet_name = "Mercury";
         break;
 
     case 2:
         weight = weight * 0.91;
-        std::cout << weight << "\n";
+        planet_name = "Venus";
         break;
 
     case 3:
         weight = weight * 0.38;
-        std::cout << weight << "\n";
+        planet_name = "Mars";
         break;
 
     case 4:
         weight = weight * 2.34;
-        std::cout << weight << "\n";
+        planet_name = "Jupiter";
         break;
 
     case 5:
         weight = weight * 1.06;
-        std::cout << weight << "\n";
+        planet_name = "Saturn";
         break;
 
     case 6:
         weight = weight * 0.92;
-        std::cout << weight << "\n";
+        planet_name = "Uranus";
         break;
 
     case 7:
         weight = weight * 1.19;
-        std::cout << weight << "\n";
+        planet_name = "Neptune";
         break;
 
     default:
         std::cout << "Invalid input.\n";
-        break;
+        return 0;
     }
+
+    std::cout << "\nYour weight in " << planet_name << " is " << weight << " units.\n\n\n";
 }
