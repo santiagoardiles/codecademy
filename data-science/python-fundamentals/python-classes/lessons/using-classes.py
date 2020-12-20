5da3113ed17e8b948f1e6fde3004741afc0e378b
Santiago Andres Ardiles Paez <santiagoandres@outlook.de>
2020-12-20 21:16:58 +0100

    Python classes.

---
+++ data-science/python-fundamentals/python-classes/lessons/using-classes.py
@@ -0,0 +1,20 @@
+# ----- Codecademy ----- #
+
+# Python Fundamentals
+# Python Dictionaries
+# Lesson: Using Dictionaries
+
+
+# --------------- Methods with Arguments --------------- #
+class Circle:
+    pi = 3.14
+
+    def area(self, radius):
+        return self.pi * radius ** 2
+
+
+circle = Circle()
+
+pizza_area = circle.area(12 / 2)
+teaching_table_area = circle.area(36 / 2)
+round_room_area = circle.area(11460 / 2)
