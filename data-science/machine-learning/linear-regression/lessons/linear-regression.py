487d6f36aa0ae15466c389b4ea9d6ba0f5f9cedc
Santiago Andrés Ardiles Páez <santiagoandres@outlook.de>
2021-01-13 18:11:51 +0100

    Step function.

--- data-science/machine-learning/linear-regression/lessons/linear-regression.py
+++ data-science/machine-learning/linear-regression/lessons/linear-regression.py
@@ -91,20 +91,6 @@
 """
     # Gradient Descent for Slope.
 
-    def get_gradient_at_b(x, y, m, b):
-        diff = 0
-        N = len(x)
-
-        for i in range(N):
-            y_val = y[i]
-            x_val = x[i]
-            diff += (y_val - ((m * x_val) + b))
-
-        b_gradient = -2/N * diff
-
-        return b_gradient
-
-
     def get_gradient_at_m(x, y, m, b):
         diff = 0
         N = len(x)
@@ -118,4 +104,33 @@
         m_gradient = -2/N * diff
 
         return m_gradient
+"""
+
+
+""" 
+    # Step function.
+
+    def step_gradient(x, y, b_current, m_current):
+        b_gradient = get_gradient_at_b(x, y, b_current, m_current)
+        m_gradient = get_gradient_at_m(x, y, b_current, m_current)
+
+        b = b_current - (0.01 * b_gradient)
+        m = m_current - (0.01 * m_gradient)
+
+        return [b, m]
+
+
+    months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    revenue = [52, 74, 79, 95, 115, 110, 129, 126, 147, 146, 156, 184]
+
+    # Current intercept guess.
+    b = 0
+
+    # Current slope guess.
+    m = 0
+
+    # Updates `b` and `m`.
+    b, m = step_gradient(months, revenue, b, m)
+
+    print(b, m)
 """
