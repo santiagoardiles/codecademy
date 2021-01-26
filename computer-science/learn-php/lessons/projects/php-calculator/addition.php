da5b0fcc824cbc57ac0ec2b2e26699ede8257c4c
santiagoardiles <santiagoandres@outlook.de>
2021-01-26 23:24:45 +0100

    Simple PHP calculator.

---
+++ computer-science/learn-php/lessons/projects/php-calculator/addition.php
@@ -0,0 +1,9 @@
+<html>
+
+<body>
+    <?= $_GET['add_first'] + $_GET['add_second'] ?>
+
+    <a href="index.php">Reset</a>
+</body>
+
+</html>
---
+++ computer-science/learn-php/lessons/projects/php-calculator/index.php
@@ -0,0 +1,23 @@
+<html>
+
+<body>
+    <!-- Addition. -->
+    <h3>Addition</h3>
+    <form method="get" action="addition.php">
+        First Number: <input name="add_first" type="number"><br>
+        Second Number: <input name="add_second" type="number"><br>
+        <button type="submit">Add</button><br>
+    </form>
+
+    <!-- Division. -->
+    <h3>Division</h3>
+    <form method="get" action="division.php">
+        Numerator: <input name="numerator" type="number"><br>
+        Denominator: <input name="denominator" type="number"><br>
+        <button type="submit">Divide</button><br>
+    </form>
+
+    <a href="index.php">Reset</a>
+</body>
+
+</html>
