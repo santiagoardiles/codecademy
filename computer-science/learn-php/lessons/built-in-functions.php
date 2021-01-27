356f34126a39daa1a23f497fd9954495dad8098e
santiagoardiles <santiagoandres@outlook.de>
2021-01-27 23:16:45 +0100

    PHP built-in functions.

---
+++ computer-science/learn-php/lessons/built-in-functions.php
@@ -0,0 +1,26 @@
+<?php
+// --------------- Codecademy. --------------- //
+//                                             //
+//     PATH: Computer Science.                 //
+//     TRACK: Learn PHP.                       //
+//     LESSON: PHP Built-in Functions.         //
+//                                             //
+// --------------- Codecademy. --------------- //
+
+
+// ------- Exercise 1. ------- //
+$first = "Welcome to the magical world of built-in functions.";
+
+$second = 82137012983;
+
+echo gettype($first);
+echo gettype($second);
+
+echo var_dump($first);
+echo var_dump($second);
+
+
+// ------- Exercise 2. ------- //
+echo strrev(".pu ti peeK .taerg gniod er'uoY");
+echo strtolower("SOON, tHiS WILL Look NoRmAL.");
+echo str_repeat("\nThere's no place like home.\n", 3);
